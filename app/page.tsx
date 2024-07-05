8c9b7b3fa7b274c5f00aa58e955195f49c27433b
Jordi Luis Fernández <jordi.ff97@gmail.com>
2024-07-05 21:16:36 +0200

    feature: add button fot start with google

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,34 @@
 "use client";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { authenticateWithGoogle } from "@/app/lib/actions";
+import { useState } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState("");
+
+  const handleGoogleSignIn = async () => {
+    setError("");
+    setIsPending(true);
+    try {
+      const user = await authenticateWithGoogle();
+      if (user) {
+        console.log("Usuario autenticado con Google:", user.email);
+        await fetch("/api/login", {
+          headers: {
+            Authorization: `Bearer ${user.idToken}`,
+          },
+        });
+        router.push("/dashboard");
+      }
+    } catch (error) {
+      setError("Failed to sign in with Google");
+    }
+    setIsPending(false);
+  };
+
   return (
     <div className="relative flex flex-col min-h-screen bg-animated-gradient">
       <header className="relative bg-transparent p-6 z-10">
@@ -31,7 +58,15 @@ export default function Home() {
           >
             Empezar Ahora
           </Link>
+          <button
+            onClick={handleGoogleSignIn}
+            className="bg-blue-600 text-white px-6 py-2 rounded-full text-lg hover:bg-blue-700 transition"
+            disabled={isPending}
+          >
+            {isPending ? "Cargando..." : "Empezar con Google"}
+          </button>
         </div>
+        {error && <p className="text-red-500 mt-4">{error}</p>}
       </main>
       <footer className="relative bg-white shadow z-10">
         <div className="container mx-auto px-4 py-4 text-center">
