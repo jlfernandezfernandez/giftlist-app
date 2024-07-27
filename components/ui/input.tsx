66af4921d3d60ce0c9285e211ffe8c99c9fa410a
Jordi Luis Fernández <jordi.ff97@gmail.com>
2024-07-27 12:57:23 +0200

    feature: fix input style

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -9,12 +9,12 @@ export interface InputProps
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, labelRight, ...props }, ref) => {
     return (
-      <div className="relative flex items-stretch">
+      <div className="relative flex w-full">
         <input
           type={type}
           className={cn(
             "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-            labelRight && "rounded-r-none border-r-0",
+            labelRight && "rounded-r-none",
             className
           )}
           ref={ref}
