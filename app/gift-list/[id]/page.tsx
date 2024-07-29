97920d9b1df2ceab0d722b1b71c2142825c51338
Jordi Luis Fernández <jordi.ff97@gmail.com>
2024-07-29 17:27:30 +0200

    feature: remove toast

--- app/gift-list/[id]/page.tsx
+++ app/gift-list/[id]/page.tsx
@@ -22,16 +22,6 @@ export default function GiftListPage() {
 
   const selectedList = giftLists?.find((list) => list.id === currentListId);
 
-  useEffect(() => {
-    if (!selectedList && giftLists) {
-      addToast({
-        title: "Error",
-        description: "Gift list not found.",
-      });
-      router.push("/gift-list/create");
-    }
-  }, []);
-
   return (
     <div className="flex min-h-screen w-full flex-col md:flex-row">
       <main className="flex-1 p-2 sm:p-6">
