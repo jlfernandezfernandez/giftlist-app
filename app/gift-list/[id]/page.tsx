4caa11275a002edb77f1142481ea80597dab2113
Jordi Luis Fernández <jordi.ff97@gmail.com>
2024-07-29 17:25:37 +0200

    feature: add toast if gift-list is not found

--- app/gift-list/[id]/page.tsx
+++ app/gift-list/[id]/page.tsx
@@ -7,15 +7,30 @@ import { useUser } from "@/context/user-context";
 import { useGiftLists } from "@/hooks/use-gift-lists";
 import { useGifts } from "@/hooks/use-gifts";
 import { useCurrentGiftListId } from "@/hooks/use-current-gift-list-id";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { useToast } from "@/context/toast-context";
 
 export default function GiftListPage() {
   const { user } = useUser();
   const currentListId = useCurrentGiftListId();
+  const router = useRouter();
+  const { addToast } = useToast();
 
   const { giftLists } = useGiftLists(user?.uid);
   const { gifts } = useGifts(currentListId);
 
   const selectedList = giftLists?.find((list) => list.id === currentListId);
+
+  useEffect(() => {
+    if (!selectedList && giftLists) {
+      addToast({
+        title: "Error",
+        description: "Gift list not found.",
+      });
+      router.push("/gift-list/create");
+    }
+  }, []);
 
   return (
     <div className="flex min-h-screen w-full flex-col md:flex-row">
